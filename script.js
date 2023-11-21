059f8d91cca76ff35f23272f44578d2438c5c1dc
Nazir Knuckles <nazirknuckles@gmail.com>
2023-11-21 06:39:09 -0500

    Completed my Personal Tech Assistant Application!

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 // llms (Large language Models)
 const { OpenAI } = require('langchain/llms/openai');
 const { PromptTemplate } = require('langchain/prompts');
-const { StructuredOutputParser } = require('langchain/output_parsers');
 const inquirer = require('inquirer');
 require('dotenv').config();
 
-// The rule the transformer has to abide by
-const promptMessage = `You are a javascript expert and will answer the user’s coding questions thoroughly as possible if the question is not related to the topic of javascript respond with "Sorry Naz not related to Javascript"\n Question: {question}`;
+const topics = 'Programming, Computer Science, Mathmatics, Artificial Intelligence';
+
+
+// The rule the transformer has to abide by using few-shot-prompting technique
+const promptMessage = `
+You are a tech expert and will answer the users question thoroughly about anything related to ${topics}, and so on.
+
+If the question is not related to ${topics}, or anything tech related say "Sorry your response has to be related to ${topics}"\n
+
+Question: {question}
+`;
 
 
 // Instantiating a new AI Large Learning Model / Transformer Model
@@ -21,7 +29,8 @@ const model = new OpenAI({
 // Creating a prompt for the Transformer Model to abide by and passing in user data
 const prompt = new PromptTemplate({
     template: promptMessage,
-    inputVariables: ['question']
+    inputVariables: ['question'],
+    // partialVariables: { format_instructions: formatInstructions }
 })
 
 
@@ -30,35 +39,29 @@ async function generateResponse(userQuestion) {
     try {
         const modelResponse = await prompt.format({ question: userQuestion });
         const res = await model.call(modelResponse);
-        console.log(res);
+        console.log(JSON.stringify(res));
     } catch (error) {
         console.error(error);
     }
 }
 
 
-const d = StructuredOutputParser.fromNamesAndDescriptions({
-    code: "Javascript code that answers the user's question",
-    explanation: 'detailed explanation of the example code provided'
-})
-
-
-
-
-
-// generateResponse('Who invented JS?');
-
 // Prompting for User input
-// const init = () => {
-//     inquirer.prompt([
-//         {
-//             type: 'input',
-//             name: 'name',
-//             message: 'Ask a coding question: '
-//         }
-//     ])
-//     .then(res => {
-//     })
-// }
+const init = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Hello! I am your Personal Tech Assistant. You can Ask me anything about Tech!:\n\n'
+        }
+    ])
+    .then(res => {
+        if(res.name != 'quit') {
+            generateResponse(res.name);
+            init();
+        }
+        else return;
+    })
+}
 
-// init();
+init();
